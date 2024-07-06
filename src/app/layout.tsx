5afcdaaeb39a404db24aa8cec085f2441e88b32a
Vaishnavi02k <85024452+Vaishnavi02k@users.noreply.github.com>
2024-07-06 14:25:58 +0530

    Admin Login Page

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import {Providers} from "./providers";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Vaishnavi Travell planner ",
+  description: "The app is powered by Bright data",
+};
+
+export default function RootLayout({children}: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import { Button } from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    <Button>Hey</Button>
+  );
+}
